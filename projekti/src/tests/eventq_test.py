014af2b716ed174e755262a0af1161fe6c7f9134
Visa Blad <visa.blad@helsinki.fi>
2021-04-20 23:09:42 +0300

    lisätty vahingossa poistetut testit

---
+++ projekti/src/tests/eventq_test.py
@@ -0,0 +1,36 @@
+import unittest
+import pygame
+from event_queue import Eventqueue
+
+
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.eventqueue = Eventqueue(40)
+        e2 = pygame.event.Event(pygame.KEYDOWN, key=275)
+        e3 = pygame.event.Event(pygame.KEYDOWN, key=273)
+        e4 = pygame.event.Event(pygame.KEYDOWN, key=276)
+        self.event_list = [e2,e3,e4]
+
+
+    def test_event_queue_accepts_events_returns_true(self):
+        state = self.eventqueue.get_event(self.event_list)
+        self.assertEqual(True, state)
+
+    def test_event_queue_returns_true_when_no_events(self):
+        state = self.eventqueue.get_event(())
+        self.assertEqual(True, state)
+    
+    def test_event_queue_returns_false_when_event_type_quit(self):
+        state = self.eventqueue.get_event([pygame.event.Event(pygame.QUIT)])
+        self.assertEqual(False, state)
+
+# eventqueuen testi antaa vaihtelevia tuloksia suorituskerrasta riippuen täytyy testata mikä juttu? 
+#<Event(2-KeyDown {'unicode': '', 'key': 275, 'mod': 0, 'scancode': 114, 'window': None})>
+    def test_event_queue_pops_returns_tuple(self):
+        eventqueue = [pygame.event.Event(pygame.KEYDOWN, key=274, scancode=116, mod=0)]
+        self.eventqueue.get_event(self.event_list)
+        state = isinstance(self.eventqueue.return_event(), tuple)
+        self.assertEqual(True, state)
+
---
+++ projekti/src/tests/snake_test.py
@@ -0,0 +1,29 @@
+import unittest
+import pygame
+from snake import Snake
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        self.snake_group = pygame.sprite.Group(Snake(40, (120,120), 5 ,(0,40)))
+        self.snake_bit = Snake(40, (120,120), 2 ,(0,40))
+    def test_snake_will_update(self):
+        for _ in range(3):
+            for snake in self.snake_group:
+                tulos = snake.update()
+        self.assertEqual(2, tulos)
+
+    def test_snake_bit_is_still_alive(self):
+        snake = Snake(40,(30,30),4,(40,0))
+        group = pygame.sprite.Group(snake)
+        group.update()
+        is_alive = pygame.sprite.Sprite.alive(snake)
+        self.assertEqual(True, is_alive)
+
+    def test_snake_bit_is_dead(self):
+        snake = Snake(40,(30,30),4,(40,0))
+        group = pygame.sprite.Group(snake)
+        for i in range(6):
+            group.update()
+        is_alive = pygame.sprite.Sprite.alive(snake)
+        self.assertEqual(False, is_alive)
